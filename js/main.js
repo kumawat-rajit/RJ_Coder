353c999b83eef8d3d7d17e2b29773aab911f38c4
Rajit <kumawat.rajit@gmail.com>
2020-01-30 17:13:11 +0530

    in progress

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('hi');
+var slideIndex =1;
+
+showSlides(slideIndex);
+
+function showSlides(n)
+{
+	var i;
+	var slides = document.getElementsByClassName('mySlides');
+	//console.log(slides.length);
+	if(n>slides.length)
+	{
+		
+		slideIndex=1;
+	}
+	if(n < slides.length)
+	{ 
+		slideIndex = slides.length;
+	}
+
+	for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+	//slides[slideIndex-1].style.display="block";
+	slides[slideIndex-1].style.display = "block";  
+}
